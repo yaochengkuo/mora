d4930378cc1764fad53b2394ecd8890d5af44fb6
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2023-03-09 16:46:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == "0") {
+    	
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(87)
+    basic.showString("hi!87")
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+radio.setGroup(69)
+basic.forever(function () {
+	
+})
